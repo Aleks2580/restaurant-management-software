b783122e6b32ac91a273d70c3ff2390c523197c5
Aleksei Vlasov <aleks101989@mail.ru>
2022-11-25 22:27:02 +0800

    main page release 0.5

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -8,6 +8,7 @@ import {
   PieChartOutlined,
 } from "@ant-design/icons";
 import { Button, Menu } from "antd";
+import style from "./Main.module.css";
 
 function getItem(label, key, icon, children, type) {
   return {
@@ -29,28 +30,30 @@ const Main = () => {
     setCollapsed(!collapsed);
   };
   return (
-    <div
-      style={{
-        width: 256,
-      }}
-    >
-      <Button
-        type="primary"
-        onClick={toggleCollapsed}
+    <div className={style.main}>
+      <div
         style={{
-          marginBottom: 16,
+          width: 256,
         }}
       >
-        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-      </Button>
-      <Menu
-        defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1"]}
-        mode="inline"
-        theme="dark"
-        inlineCollapsed={collapsed}
-        items={items}
-      />
+        <Button
+          type="primary"
+          onClick={toggleCollapsed}
+          style={{
+            marginBottom: 16,
+          }}
+        >
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </Button>
+        <Menu
+          // defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+          items={items}
+        />
+      </div>
     </div>
   );
 };
