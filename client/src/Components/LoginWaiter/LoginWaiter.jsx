a0cecd36344b46c86df6bb0d2e424c716a0c740a
Aleksei Vlasov <aleks101989@mail.ru>
2022-11-28 20:03:21 +0800

    inputHandler

--- client/src/Components/LoginWaiter/LoginWaiter.jsx
+++ client/src/Components/LoginWaiter/LoginWaiter.jsx
@@ -1,46 +1,17 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
 import style from "./LoginWaiter.module.css";
+import { useState } from "react";
 
 export default function Waiter() {
+  const [input, setInput] = useState("");
+  const inputHandler = (e) => {
+    setInput((prev) => prev + e.target.innerText);
+  };
+
   return (
     <div className={style.waiter}>
       <span>Enter your password</span>
-      <div className={style.digits}>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          1
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          2
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          3
-        </Button>
-
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          4
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          5
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          6
-        </Button>
-
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          7
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          8
-        </Button>
-        <Button className={style.button_waiter} type="primary" shape="circle">
-          9
-        </Button>
-
-        <Button className={style.zero} type="primary" shape="circle">
-          0
-        </Button>
-      </div>
 
       <Form
         name="basic"
@@ -49,6 +20,41 @@ export default function Waiter() {
         initialValues={{ remember: true }}
         autoComplete="off"
       >
+        <div className={style.digits}>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            1
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            2
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            3
+          </Button>
+
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            4
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            5
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            6
+          </Button>
+
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            7
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            8
+          </Button>
+          <Button className={style.button_waiter} type="primary" shape="circle">
+            9
+          </Button>
+
+          <Button className={style.zero} type="primary" shape="circle">
+            0
+          </Button>
+        </div>
         <Form.Item
           name="password"
           rules={[{ required: true, message: "Input your password!" }]}
