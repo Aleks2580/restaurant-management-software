7cbeb173a41e993786287eb1650baefeea624e6b
Aleksei Vlasov <aleks101989@mail.ru>
2022-11-29 22:43:20 +0800

    refactoring loginwaiter component + added reset button

--- client/src/Components/LoginWaiter/LoginWaiter.jsx
+++ client/src/Components/LoginWaiter/LoginWaiter.jsx
@@ -1,71 +1,72 @@
 import React from "react";
-import { Button, Form, Input } from "antd";
+import { Button, Form } from "antd";
 import style from "./LoginWaiter.module.css";
 import { useState } from "react";
+import { digits, waiters } from "./mockdata";
 
 export default function Waiter() {
   const [input, setInput] = useState("");
   const inputHandler = (e) => {
     setInput((prev) => prev + e.target.innerText);
   };
-
+  console.log(input);
+  const resetHandler = () => {
+    setInput("");
+  };
+  const submitHandler = () => {
+    waiters.password === input ? console.log("OK") : console.log("Denied!");
+  };
   return (
     <div className={style.waiter}>
-      <span>Enter your password</span>
-
       <Form
+        className={style.form}
         name="basic"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         initialValues={{ remember: true }}
         autoComplete="off"
       >
+        <span>Enter your 6 digit password</span>
         <div className={style.digits}>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            1
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            2
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            3
-          </Button>
-
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            4
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            5
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            6
-          </Button>
-
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            7
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            8
-          </Button>
-          <Button className={style.button_waiter} type="primary" shape="circle">
-            9
-          </Button>
-
-          <Button className={style.zero} type="primary" shape="circle">
-            0
-          </Button>
+          {digits.map((el) => (
+            <Button
+              onClick={inputHandler}
+              className={style.button_waiter}
+              type="primary"
+              shape="circle"
+            >
+              {el}
+            </Button>
+          ))}
         </div>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Input your password!" }]}
-        >
-          <Input.Password className={style.input_waiter} />
-        </Form.Item>
+        <div>
+          <input
+            className={style.input_waiter}
+            value={input
+              .split("")
+              .map((el) => (el = "*"))
+              .join("")}
+          />
+        </div>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
+        <Form.Item>
+          <div className={style.form_buttons}>
+            <Button
+              onClick={submitHandler}
+              className={style.button}
+              type="primary"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+            <Button
+              onClick={resetHandler}
+              className={style.button_reset}
+              type="primary"
+            >
+              Reset
+            </Button>
+          </div>
         </Form.Item>
       </Form>
     </div>
---
+++ client/src/Components/LoginWaiter/mockdata.js
@@ -0,0 +1,6 @@
+export const digits = Array.from(Array(10).keys());
+
+export const waiters = {
+  name: 'Mark',
+  password: '425800'
+}
