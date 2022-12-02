dc4eb64ecb1bc0501244ff2090246895a012947d
Aleksei Vlasov <aleks101989@mail.ru>
2022-12-02 13:49:13 +0800

    layout MainWaiter component

--- client/src/Components/MainWaiter/MainWaiter.jsx
+++ client/src/Components/MainWaiter/MainWaiter.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
-export default function MainWaiter() {
-  return <div>MainWaiter</div>;
-}
+import { Layout } from "antd";
+import style from "./MainWaiter.module.css";
+const { Header, Footer, Sider, Content } = Layout;
+const MainWaiter = () => (
+  <Layout className={style.layout}>
+    <Sider>Sider</Sider>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  </Layout>
+);
+export default MainWaiter;
