afc2289b869e1bc874dc5eeae5680eefdce782da
Aleksei Vlasov <aleks101989@mail.ru>
2022-12-05 16:18:59 +0800

    main manager component styling

--- client/src/Components/MainManager/MainManager.jsx
+++ client/src/Components/MainManager/MainManager.jsx
@@ -6,10 +6,13 @@ import {
 } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 import style from "./MainManager.module.css";
+import { StyleProvider } from "@ant-design/cssinjs";
 const { Header, Content, Footer, Sider } = Layout;
+
 const MainManager = () => (
   <Layout className={style.layout}>
     <Sider
+      className={style.sider}
       breakpoint="lg"
       collapsedWidth="0"
       onBreakpoint={(broken) => {
@@ -21,7 +24,8 @@ const MainManager = () => (
     >
       <div className="logo" />
       <Menu
-        theme="dark"
+        className={style.menu}
+        theme="light"
         mode="inline"
         defaultSelectedKeys={["4"]}
         items={[
@@ -36,20 +40,26 @@ const MainManager = () => (
         }))}
       />
     </Sider>
-    <Layout>
+    <Layout className={style.layout_secondary}>
       <Header
-        className="site-layout-sub-header-background"
+        className={style.header}
+        // className="site-layout-sub-header-background"
         style={{
           padding: 0,
         }}
-      />
+      >
+        <div className={style.header_div}>
+          <span className={style.welcome}>Welcome, Admin!</span>
+          <span className={style.time}>16:46</span>
+        </div>
+      </Header>
       <Content
         style={{
           margin: "24px 16px 0",
         }}
       >
         <div
-          className="site-layout-background"
+          className={style.content}
           style={{
             padding: 24,
             minHeight: 360,
@@ -59,12 +69,11 @@ const MainManager = () => (
         </div>
       </Content>
       <Footer
+        className={style.footer}
         style={{
           textAlign: "center",
         }}
-      >
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
+      ></Footer>
     </Layout>
   </Layout>
 );
