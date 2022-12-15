356590b2d0966acd2a5fe19a4fdbcea25b806799
Aleksei Vlasov <aleks101989@mail.ru>
2022-12-15 18:36:11 +0800

    new user form

--- client/src/Components/Manager/UsersManager/NewUserForm.jsx
+++ client/src/Components/Manager/UsersManager/NewUserForm.jsx
@@ -1,5 +1,79 @@
 import React from "react";
+import { Button, Form, Input } from "antd";
 
-export default function NewUserForm() {
-  return <div>NewUserForm</div>;
-}
+const App = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="fullName"
+        rules={[
+          {
+            required: true,
+            message: "Please input name!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="Role"
+        name="role"
+        rules={[
+          {
+            required: true,
+            message: "Please input role!",
+          },
+        ]}
+      >
+        <Input placeholder="manager or waiter" />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default App;
