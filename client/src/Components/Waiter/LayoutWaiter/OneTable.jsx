2ae03dc52c27870a768a337f4cc9c16830d02a0a
Aleksei Vlasov <aleks101989@mail.ru>
2022-12-20 10:50:07 +0800

    modal when table is clicked

--- client/src/Components/Waiter/LayoutWaiter/OneTable.jsx
+++ client/src/Components/Waiter/LayoutWaiter/OneTable.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import style from "./OneTable.module.css";
 import { useState } from "react";
+import { Modal } from "antd";
 
 export default function OneTable({ el }) {
-  return <div className={style.table_first_row}>{el.number}</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOk = () => {};
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const createOrderHandler = () => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <Modal
+        className={style.modal}
+        title="New order"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Create a new order?</p>
+      </Modal>
+      <div onClick={createOrderHandler} className={style.table_first_row}>
+        {el.number}
+      </div>
+    </>
+  );
 }
