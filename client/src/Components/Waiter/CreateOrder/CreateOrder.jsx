df1aa01cdfc9586cd69d83dc4a28d9a096bf4af0
Aleksei Vlasov <aleks101989@mail.ru>
2023-01-10 15:17:36 +0800

    create_order:waiter, tableNum, guests

--- client/src/Components/Waiter/CreateOrder/CreateOrder.jsx
+++ client/src/Components/Waiter/CreateOrder/CreateOrder.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { InputNumber } from "antd";
+import { useSelector } from "react-redux";
+import { getTable } from "../../../store/createOrder/actionCreators";
 
 export default function CreateOrder() {
-  return <div>CreateOrder</div>;
+  const waiter = useSelector((state) => state.loginUser.name);
+  const tableNumber = useSelector((state) => state.createOrder);
+  console.log(tableNumber);
+  const onChange = (value) => {
+    console.log("changed", value);
+  };
+  return (
+    <div>
+      <div>Waiter: {waiter}</div>
+      <div>Table number:{tableNumber}</div>
+      <div>
+        Number of guests:
+        <InputNumber min={1} max={30} defaultValue={2} onChange={onChange} />
+      </div>
+    </div>
+  );
 }
