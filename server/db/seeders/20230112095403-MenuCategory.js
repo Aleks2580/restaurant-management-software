1fc94a1a1bf9c1c77c322f82210f959f85f018ca
Aleksei Vlasov <aleks101989@mail.ru>
2023-01-12 22:59:12 +0800

    menu categories seeded

--- server/db/seeders/20230112095403-MenuCategory.js
+++ server/db/seeders/20230112095403-MenuCategory.js
@@ -3,23 +3,75 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(
+      'MenuCategories',
+      [
+        {
+          name: 'Salads',
+          menuSectionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Soups',
+          menuSectionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Main',
+          menuSectionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Pizza',
+          menuSectionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Desserts',
+          menuSectionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Tea',
+          menuSectionId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Coffee',
+          menuSectionId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Cocktails',
+          menuSectionId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Soft drinks',
+          menuSectionId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Wine',
+          menuSectionId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete('MenuCategories', null, {});
+  },
 };
