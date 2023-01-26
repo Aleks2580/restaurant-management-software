aa73bfb5cf0766d3581881047738307261c24925
Aleksei Vlasov <aleks101989@mail.ru>
2023-01-26 16:30:56 +0800

    model order created

---
+++ server/db/migrations/20230126082952-create-order.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      waiterName: {
+        type: Sequelize.STRING
+      },
+      waiterId: {
+        type: Sequelize.INTEGER
+      },
+      tableNumber: {
+        type: Sequelize.INTEGER
+      },
+      tableId: {
+        type: Sequelize.INTEGER
+      },
+      guests: {
+        type: Sequelize.INTEGER
+      },
+      items: {
+        type: Sequelize.TEXT
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
