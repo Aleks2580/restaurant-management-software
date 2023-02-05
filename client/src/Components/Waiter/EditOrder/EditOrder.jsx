87d6dd75fd217cc851260d60ff198a2bb76823a5
Aleksei Vlasov <aleks101989@mail.ru>
2023-02-05 13:03:51 +0800

    get Items for editing order

--- client/src/Components/Waiter/EditOrder/EditOrder.jsx
+++ client/src/Components/Waiter/EditOrder/EditOrder.jsx
@@ -3,10 +3,12 @@ import style from "./EditOrder.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { InputNumber, Button, message } from "antd";
+import { PlusOutlined, MinusOutlined, DeleteOutlined } from "@ant-design/icons";
 
 export default function EditOrder() {
   const [sections, setSections] = useState([{ name: "" }, { name: "" }]);
-  const [guestsNumber, setGuestsNumber] = useState();
+  const [guestsNumber, setGuestsNumber] = useState(0);
+  const [order, setOrder] = useState([]);
   const dispatch = useDispatch();
   const waiterName = useSelector((state) => state.loginUser.name);
   const tableNumber = useSelector((state) => state.viewOrder);
@@ -34,17 +36,38 @@ export default function EditOrder() {
         credentials: "include",
       });
       const result = await response.json();
-      console.log(result);
       setGuestsNumber(result.order.guests);
-      //setOrder(result.order);
+      setOrder(result.order);
     })();
-  }, [tableNumber]);
+  }, []);
+  //console.log([order]);
 
   const onChange = (value) => {
     setGuestsNumber(value);
   };
 
-  console.log(guestsNumber);
+  const handleDelete = (e, index, price, quantity) => {
+    // let totalSum = price * quantity;
+    // dispatch(deleteItem(index));
+    // dispatch(subtractTotal(totalSum));
+  };
+
+  const handleAdd = (e, index, price) => {
+    // dispatch(addItem(index));
+    // dispatch(addTotal(price));
+  };
+
+  const handleSubtract = (e, index, price, quantity) => {
+    // if (quantity === 1) {
+    //   dispatch(deleteItem(index));
+    //   dispatch(subtractTotal(price));
+    // } else {
+    //   dispatch(subtractItem(index));
+    //   dispatch(subtractTotal(price));
+    // }
+  };
+
+  //console.log(guestsNumber);
 
   return (
     <>
@@ -91,6 +114,43 @@ export default function EditOrder() {
             Done
           </Button>
         </div>
+      </div>
+      <div className={style.main_order}>
+        <div className={style.order}>
+          ORDER:
+          <div className={style.name_amount_price}>
+            <div className={style.name}>Name</div>
+            <div className={style.quantity}>Quantity</div>
+            <div className={style.price}>Price</div>
+            <div className={style.total}>Total</div>
+          </div>
+          {order.items?.map((el, index) => (
+            <div className={style.item}>
+              <div className={style.item_name}>{el.item}</div>
+              <div className={style.item_quantity}>{el.quantity}</div>
+              <div className={style.item_price}>{el.price}$</div>
+              <div className={style.item_total}>{el.price * el.quantity}$</div>
+              <div className={style.icons}>
+                <PlusOutlined
+                  className={style.icon}
+                  onClick={(e) => handleAdd(e, index, el.price)}
+                />
+                <MinusOutlined
+                  className={style.icon}
+                  onClick={(e) =>
+                    handleSubtract(e, index, el.price, el.quantity)
+                  }
+                />
+                <DeleteOutlined
+                  onClick={(e) => handleDelete(e, index, el.price, el.quantity)}
+                  className={style.icon}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className={style.total_sum}>TOTAL: total$</div>
       </div>
     </>
   );
