b87ad7c5dc958499305d3e584ada569c730eb131
Aleksei Vlasov <aleks101989@mail.ru>
2023-02-22 19:06:30 +0800

    all products set

--- client/src/Components/Manager/ProductsManager/AllProducts.jsx
+++ client/src/Components/Manager/ProductsManager/AllProducts.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import OneProduct from "./OneProduct";
 
 export default function AllProducts() {
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState();
+  useEffect(() => {
+    (async function () {
+      const response = await fetch("http://localhost:4000/products", {
+        method: "GET",
+        credentials: "include",
+      });
+      const result = await response.json();
+      //setUsers(result.users);
+      //setLoading(false);
+    })();
+  }, []);
   return <div>AllProducts</div>;
 }
