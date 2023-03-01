ea575832f02b78c1068131dc439aeb394c06638e
Aleksei Vlasov <aleks101989@mail.ru>
2023-03-01 12:03:28 +0800

    existing categories in new category component

--- client/src/Components/Manager/ProductsManager/NewCategory.jsx
+++ client/src/Components/Manager/ProductsManager/NewCategory.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import style from "./NewSection.module.css";
+import { Input, Button, message } from "antd";
 
 export default function NewCategory() {
-  return <div>NewCategory</div>;
+  const [categories, setCategories] = useState();
+  const [sections, setSections] = useState();
+  const [input, setInput] = useState({ name: "" });
+  const [submitClicked, setSubmitClicked] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch("http://localhost:4000/menu_categories", {
+        method: "GET",
+        credentials: "include",
+      });
+      const result = await response.json();
+      setCategories(result.categories);
+    })();
+  }, []);
+
+  return (
+    <>
+      <div className={style.main}>
+        <div className={style.existing_sections}>
+          <div className={style.text}>Existing sections:</div>
+          <div className={style.sections}>
+            {categories?.map((category) => (
+              <span className={style.section}>{category.name}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={style.form}>
+        <Input
+          //onChange={handleInput}
+          name="name"
+          placeholder="name of the new section"
+          value={input.name}
+          className={style.input}
+        />
+        <Button
+          //onClick={handleSubmit}
+          className={style.button}
+          htmlType="submit"
+        >
+          Submit
+        </Button>
+      </div>
+    </>
+  );
 }
--- server/src/routes/menuCategories.js
+++ server/src/routes/menuCategories.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const { MenuCategory } = require('../../db/models');
 
+router.get('/', async (req, res) => {
+  try {
+    const categories = await MenuCategory.findAll({ raw: true });
+    res.json({ categories });
+  } catch (error) {
+    res.send(`Error while loading categories! ${error}`);
+  }
+});
+
+module.exports = router;
+
+
 router.post('/', async (req, res) => {
   const menuSectionId = +req.body.id;
   try {
