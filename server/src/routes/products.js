185f6593ee5d06a6aa59be9d8976562767453d74
Aleksei Vlasov <aleks101989@mail.ru>
2023-03-27 16:25:44 +0700

    styling all products

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -5,35 +5,10 @@ const { Item, MenuSection, MenuCategory } = require("../../db/models");
 router.get("/", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 20;
-  console.log("PAGE", page);
   const offset = (page - 1) * pageSize;
   const limit = pageSize;
   const section = req.query.section || null;
   const category = req.query.category || null;
-  //console.log("CATEGORY SECTION", category, section);
-
-  //   const totalCount = await Item.count();
-
-  //   const products = await Item.findAll({
-  //     offset,
-  //     limit,
-  //     raw: true,
-  //     include: [
-  //       {
-  //         model: MenuSection,
-  //         attributes: ["name"],
-  //       },
-  //       {
-  //         model: MenuCategory,
-  //         attributes: ["name"],
-  //       },
-  //     ],
-  //   });
-  //   console.log("CASE0");
-  //   res.json({ products, totalCount });
-  // } catch (error) {
-  //   res.send(`Error while loading products! ${error}`);
-  // }
 
   if (section !== "all" && category !== "all") {
     try {
@@ -82,7 +57,6 @@ router.get("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE1");
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -131,7 +105,7 @@ router.get("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE2", products, categories, totalCount);
+
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -181,7 +155,7 @@ router.get("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE3");
+
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -207,7 +181,6 @@ router.get("/", async (req, res) => {
         ],
       });
       const totalCount = await Item.count();
-      console.log("CASE4");
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -215,60 +188,12 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const page = parseInt(req.query.page) || 1;
-//     const pageSize = parseInt(req.query.pageSize) || 20;
-//     const section = req.query.section || null;
-//     const category = req.query.category || null;
-//     console.log("PAGE", page);
-//     const offset = (page - 1) * pageSize;
-//     const limit = pageSize;
-
-//     let whereClause = {};
-//     if (section) {
-//       whereClause = { ...whereClause, section: section };
-//     }
-//     if (category) {
-//       whereClause = { ...whereClause, category: category };
-//     }
-
-//     const totalCount = await Item.count({
-//       where: whereClause,
-//     });
-
-//     const products = await Item.findAll({
-//       offset,
-//       limit,
-//       raw: true,
-//       where: whereClause,
-//       include: [
-//         {
-//           model: MenuSection,
-//           attributes: ["name"],
-//         },
-//         {
-//           model: MenuCategory,
-//           attributes: ["name"],
-//         },
-//       ],
-//     });
-//     console.log("CASE0");
-//     res.json({ products, totalCount });
-//   } catch (error) {
-//     //res.send(`Error while loading products! ${error}`);
-//     res.status(500).json({ error: `Error while loading products! ${error}` });
-//   }
-// });
-
 module.exports = router;
 
 router.post("/", async (req, res) => {
   const { section, category } = req.body.filter;
-  //console.log("SECTION:", section, "CATEGORY", category);
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 20;
-  console.log("BODY", req.body);
   const offset = (page - 1) * pageSize;
   const limit = pageSize;
   if (section !== "all" && category !== "all") {
@@ -318,7 +243,6 @@ router.post("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE1");
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -367,7 +291,6 @@ router.post("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE2", products, categories, totalCount);
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -417,7 +340,6 @@ router.post("/", async (req, res) => {
           },
         ],
       });
-      console.log("CASE3");
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
@@ -443,7 +365,6 @@ router.post("/", async (req, res) => {
         ],
       });
       const totalCount = await Item.count();
-      console.log("CASE4");
       res.json({ products, categories, totalCount });
     } catch (error) {
       res.send(`Error while loading products! ${error}`);
