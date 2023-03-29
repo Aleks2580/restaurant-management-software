8f1579bc6815d0770d3a236d8dcbb8497b2a4775
Aleksei Vlasov <aleks101989@mail.ru>
2023-03-29 16:07:09 +0700

    revenue component styling

--- client/src/Components/Manager/StatisticsManager/Revenue.jsx
+++ client/src/Components/Manager/StatisticsManager/Revenue.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { DatePicker, Button } from "antd";
+import { SearchOutlined, DownloadOutlined } from "@ant-design/icons";
+import style from "./Revenue.module.css";
+const { RangePicker } = DatePicker;
 
 export default function Revenue() {
-  return <div>Revenue</div>;
+  const [data, setData] = useState([]);
+
+  const handleSearchButton = async () => {};
+  return (
+    <div className={style.main_div}>
+      <div>
+        <RangePicker />
+        <Button
+          shape="circle"
+          className={style.button_search}
+          icon={<SearchOutlined />}
+          onClick={handleSearchButton}
+        />
+      </div>
+      <div className={style.data}>Data</div>
+      <div>
+        <Button className={style.button_download} icon={<DownloadOutlined />}>
+          download
+        </Button>
+      </div>
+    </div>
+  );
 }
