1ba92ae72353025188e9cfcdb143764bc0840567
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-02 18:59:54 +0700

    waiter revenue

--- client/src/Components/Manager/StatisticsManager/Waiters.jsx
+++ client/src/Components/Manager/StatisticsManager/Waiters.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DatePicker, Button, Table, message } from "antd";
 import {
   SearchOutlined,
@@ -12,5 +12,42 @@ import { saveAs } from "file-saver";
 const { RangePicker } = DatePicker;
 
 export default function Waiters() {
-  return <div>Waiters</div>;
+  const [waiters, setWaiters] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(
+        `http://localhost:4000/products?page=${page}&pageSize=${pageSize}&section=${filter.section}&category=${filter.category}`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+      const result = await response.json();
+      setProducts(result.products);
+      setLoading(false);
+      setTotal(result.totalCount);
+    })();
+  }, []);
+
+  return (
+    <div className={style.filter_div}>
+      Waiter
+      <select
+        //onChange={handleChange}
+        className={style.select}
+        name="section"
+        //value={filter.section}
+      >
+        <option className={style.option} value="all" name="section">
+          all
+        </option>
+        {/* {sections?.map((el) => (
+        <option className={style.option} value={el.name} name="section">
+          {el.name}
+        </option>
+      ))} */}
+      </select>
+    </div>
+  );
 }
