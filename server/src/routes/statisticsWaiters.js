532247538bbe49ea24c4a4f6966f74033c972c21
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-04 13:12:51 +0700

    consition for right data statistics waiters

--- server/src/routes/statisticsWaiters.js
+++ server/src/routes/statisticsWaiters.js
@@ -12,25 +12,86 @@ router.post("/", async (req, res) => {
     },
   } = req.body;
 
-  console.log(waiterName);
-
-  if (waiterName) {
+  if (waiterName === "all" && !startDate && !endDate) {
     try {
       const data = await Order.findAll({
-        // attributes: [
-        //   [
-        //     Sequelize.fn("TO_CHAR", Sequelize.col("createdAt"), "YYYY-MM-DD"),
-        //     "date",
-        //   ],
-
-        //   [Sequelize.fn("SUM", Sequelize.col("guests")), "totalGuests"],
-        //   [Sequelize.fn("SUM", Sequelize.col("total")), "totalRevenue"],
-        // ],
-
-        // group: ["waiterName"],
+        attributes: [
+          [
+            Sequelize.fn("TO_CHAR", Sequelize.col("createdAt"), "YYYY-MM-DD"),
+            "date",
+          ],
+          "waiterName",
+          [Sequelize.fn("sum", Sequelize.col("guests")), "totalGuests"],
+          [Sequelize.fn("sum", Sequelize.col("total")), "totalAmount"],
+        ],
+        group: ["date", "waiterName"],
+        order: [["date", "ASC"]],
         raw: true,
       });
-      console.log("DATA", data);
+      res.json({ data });
+    } catch (error) {
+      res.send(`Error while loading data! ${error}`);
+    }
+  } else if (waiterName !== "all" && startDate && startDate) {
+    try {
+      const data = await Order.findAll({
+        where: {
+          waiterName,
+          createdAt: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        attributes: [
+          [
+            Sequelize.fn("TO_CHAR", Sequelize.col("createdAt"), "YYYY-MM-DD"),
+            "date",
+          ],
+          "waiterName",
+          [Sequelize.fn("sum", Sequelize.col("guests")), "totalGuests"],
+          [Sequelize.fn("sum", Sequelize.col("total")), "totalAmount"],
+        ],
+        group: ["date", "waiterName"],
+        order: [["date", "ASC"]],
+        raw: true,
+      });
+      // const data = await Order.findAll({
+      //   where: { waiterName },
+      //   attributes: [
+      //     [
+      //       Sequelize.fn("TO_CHAR", Sequelize.col("createdAt"), "YYYY-MM-DD"),
+      //       "date",
+      //     ],
+      //     "waiterName",
+      //     [Sequelize.fn("sum", Sequelize.col("guests")), "totalGuests"],
+      //     [Sequelize.fn("sum", Sequelize.col("total")), "totalAmount"],
+      //   ],
+      //   group: ["date"],
+      //   order: [["date", "ASC"]],
+      //   raw: true,
+      // });
+      res.json({ data });
+    } catch (error) {
+      res.send(`Error while loading data! ${error}`);
+    }
+  } else {
+    try {
+      const data = await Order.findAll({
+        where: {
+          waiterName,
+        },
+        attributes: [
+          [
+            Sequelize.fn("TO_CHAR", Sequelize.col("createdAt"), "YYYY-MM-DD"),
+            "date",
+          ],
+          "waiterName",
+          [Sequelize.fn("sum", Sequelize.col("guests")), "totalGuests"],
+          [Sequelize.fn("sum", Sequelize.col("total")), "totalAmount"],
+        ],
+        group: ["date", "waiterName"],
+        order: [["date", "ASC"]],
+        raw: true,
+      });
       res.json({ data });
     } catch (error) {
       res.send(`Error while loading data! ${error}`);
