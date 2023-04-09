0cfdc2267d9e9020c50abc9df6bc085327788f8b
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-09 18:53:45 +0700

    styling of dasboard changed

--- server/src/routes/dashboard.js
+++ server/src/routes/dashboard.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Order, Reservation } = require('../../db/models');
+const router = require("express").Router();
+const { Order, Reservation } = require("../../db/models");
 
-router.get('/', async (req, res) => {
-  const today = (new Date().setHours(0, 0, 0, 0));
+router.get("/", async (req, res) => {
+  const today = new Date().setHours(0, 0, 0, 0);
   let activeOrdersTotal = 0;
   let activeOrdersGuests = 0;
   let activeOrdersAverageCheck = 0;
@@ -17,35 +17,51 @@ router.get('/', async (req, res) => {
   let projectedRevenue = 0;
 
   try {
-    const activeOrders = await Order.findAll({ where: { open: true }, raw: true });
+    const activeOrders = await Order.findAll({
+      where: { open: true },
+      raw: true,
+    });
     activeOrders.forEach((activeOrder) => {
       activeOrdersTotal += activeOrder.total;
       activeOrdersGuests += activeOrder.guests;
     });
-    activeOrdersAverageCheck = Math.round(activeOrdersTotal / activeOrders.length);
-    let activeOrdersTables = activeOrders.length;
+    activeOrdersAverageCheck = Math.round(
+      activeOrdersTotal / activeOrders.length
+    );
+    const activeOrdersTables = activeOrders.length;
 
     const totalOrdersFromBack = await Order.findAll({ raw: true });
-    const totalOrders = totalOrdersFromBack
-      .filter((order) => order.createdAt.toDateString() === new Date(today).toDateString());
+    const totalOrders = totalOrdersFromBack.filter(
+      (order) =>
+        order.createdAt.toDateString() === new Date(today).toDateString()
+    );
     totalOrders.forEach((order) => {
       totalOrdersTotal += order.total;
       totalOrdersGuests += order.guests;
     });
     totalOrdersAverageCheck = Math.round(totalOrdersTotal / totalOrders.length);
-    let totalOrdersTables = totalOrders.length;
+    const totalOrdersTables = totalOrders.length;
 
-    const paidOrders = totalOrdersFromBack.filter((order) => order.createdAt.toDateString() === new Date(today).toDateString() && order.open === false && order.billPrinted === true);
+    const paidOrders = totalOrdersFromBack.filter(
+      (order) =>
+        order.createdAt.toDateString() === new Date(today).toDateString() &&
+        order.open === false &&
+        order.billPrinted === true
+    );
     paidOrders.forEach((paidOrder) => {
       totalPaidOrdersTotal += paidOrder.total;
       totalPaidOrdersGuests += paidOrder.guests;
     });
 
-    totalPaidOrdersAverageCheck = Math.round(totalPaidOrdersTotal / paidOrders.length);
+    totalPaidOrdersAverageCheck = Math.round(
+      totalPaidOrdersTotal / paidOrders.length
+    );
     let totalPaidOrdersTables = paidOrders.length;
 
     const reservationsFromBack = await Reservation.findAll({ raw: true });
-    const reservations = reservationsFromBack.filter((reservation) => new Date(reservation.date).setHours(0, 0, 0, 0) === today);
+    const reservations = reservationsFromBack.filter(
+      (reservation) => new Date(reservation.date).setHours(0, 0, 0, 0) === today
+    );
 
     reservationsTables = reservations.length;
     projectedRevenue = reservationsTables * totalOrdersAverageCheck;
@@ -69,7 +85,6 @@ router.get('/', async (req, res) => {
       reservationsTables,
       projectedRevenue,
       reservationsGuests,
-
     });
   } catch (error) {
     res.send(`Error while loading data! ${error}`);
