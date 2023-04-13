dae18b6bdf5375339d79a04beb4b77fa6cbf0ece
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-13 18:39:50 +0700

    responsive home page

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import {
-  ContainerOutlined,
-  DesktopOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-} from "@ant-design/icons";
-import { Button, Menu } from "antd";
+import { DesktopOutlined, UserOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
 import style from "./Home.module.css";
 
 function getItem(label, key, icon, children, type) {
@@ -23,44 +17,25 @@ const items = [
   getItem(
     <Link to="/waiter">Waiter</Link>,
     "1",
-    <PieChartOutlined className={style.icon} />
+    <UserOutlined className={style.icon} />
   ),
   getItem(
     <Link to="/manager">Manager</Link>,
     "2",
     <DesktopOutlined className={style.icon} />
   ),
-  getItem("Option 3", "3", <ContainerOutlined className={style.icon} />),
 ];
 const Home = () => {
-  const [collapsed, setCollapsed] = useState(true);
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
-  };
-
   return (
-    <div className={style.home}>
-      <div className={style.main}>
-        <div
-          style={{
-            width: 150,
-          }}
-        >
-          <Button
-            className={style.button_menu}
-            type="primary"
-            onClick={toggleCollapsed}
-          >
-            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          </Button>
-          <Menu
-            className={style.menu}
-            defaultOpenKeys={["sub1"]}
-            mode="inline"
-            inlineCollapsed={collapsed}
-            items={items}
-          />
-        </div>
+    <div className={style.main}>
+      <div
+        style={{
+          width: 150,
+        }}
+      >
+        <Menu className={style.menu} defaultOpenKeys={["sub1"]} items={items} />
+      </div>
+      <div className={style.div_span}>
         <p className={style.popout}>RESTAURANT MANAGEMENT SYSTEM</p>
       </div>
     </div>
