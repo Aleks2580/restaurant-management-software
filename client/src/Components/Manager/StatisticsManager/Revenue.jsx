6e28f93edfb553db5c941caea9f0f871cd98e495
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-28 17:35:15 +0800

    Statisctics Revenue styling 30% done

--- client/src/Components/Manager/StatisticsManager/Revenue.jsx
+++ client/src/Components/Manager/StatisticsManager/Revenue.jsx
@@ -56,6 +56,7 @@ export default function Revenue() {
     setAverageCheckPerGuest([]);
   };
   const dataSourse = perDateData;
+  console.log(perDateData);
   const columns = [
     {
       title: "Date",
