6053133a145b115983de5dd8ffd63eb6ca9dd6ac
Aleksei Vlasov <aleks101989@mail.ru>
2023-05-01 22:28:10 +0800

    sequelize

--- server/src/routes/addUser.js
+++ server/src/routes/addUser.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const { User } = require('../../db/models');
+const router = require("express").Router();
+const { User } = require("../../db/models");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { fullName, password, role } = req.body;
-
+  console.log(fullName);
   try {
     await User.create({ fullName, password, role });
-    res.json('Done')
+    res.json("Done");
   } catch (error) {
     res.json(`Error while creating user ${error}`);
   }
